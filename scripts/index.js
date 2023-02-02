63466f194cac95533f5bfc0c20ddbcbd61f3567e
AndreyT-v <andreytumbaev@mail.ru>
2023-02-03 00:18:00 +0300

    ДЗ 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,156 @@
+/*! js-cookie v3.0.1 | MIT */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, (function () {
+    var current = global.Cookies;
+    var exports = global.Cookies = factory();
+    exports.noConflict = function () { global.Cookies = current; return exports; };
+  }()));
+}(this, (function () { 'use strict';
+
+  /* eslint-disable no-var */
+  function assign (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        target[key] = source[key];
+      }
+    }
+    return target
+  }
+  /* eslint-enable no-var */
+
+  /* eslint-disable no-var */
+  var defaultConverter = {
+    read: function (value) {
+      if (value[0] === '"') {
+        value = value.slice(1, -1);
+      }
+      return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
+    },
+    write: function (value) {
+      return encodeURIComponent(value).replace(
+        /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
+        decodeURIComponent
+      )
+    }
+  };
+  /* eslint-enable no-var */
+
+  /* eslint-disable no-var */
+
+  function init (converter, defaultAttributes) {
+    function set (key, value, attributes) {
+      if (typeof document === 'undefined') {
+        return
+      }
+
+      attributes = assign({}, defaultAttributes, attributes);
+
+      if (typeof attributes.expires === 'number') {
+        attributes.expires = new Date(Date.now() + attributes.expires * 864e5);
+      }
+      if (attributes.expires) {
+        attributes.expires = attributes.expires.toUTCString();
+      }
+
+      key = encodeURIComponent(key)
+        .replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent)
+        .replace(/[()]/g, escape);
+
+      var stringifiedAttributes = '';
+      for (var attributeName in attributes) {
+        if (!attributes[attributeName]) {
+          continue
+        }
+
+        stringifiedAttributes += '; ' + attributeName;
+
+        if (attributes[attributeName] === true) {
+          continue
+        }
+
+        // Considers RFC 6265 section 5.2:
+        // ...
+        // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+        //     character:
+        // Consume the characters of the unparsed-attributes up to,
+        // not including, the first %x3B (";") character.
+        // ...
+        stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
+      }
+
+      return (document.cookie =
+        key + '=' + converter.write(value, key) + stringifiedAttributes)
+    }
+
+    function get (key) {
+      if (typeof document === 'undefined' || (arguments.length && !key)) {
+        return
+      }
+
+      // To prevent the for loop in the first place assign an empty array
+      // in case there are no cookies at all.
+      var cookies = document.cookie ? document.cookie.split('; ') : [];
+      var jar = {};
+      for (var i = 0; i < cookies.length; i++) {
+        var parts = cookies[i].split('=');
+        var value = parts.slice(1).join('=');
+
+        try {
+          var foundKey = decodeURIComponent(parts[0]);
+          jar[foundKey] = converter.read(value, foundKey);
+
+          if (key === foundKey) {
+            break
+          }
+        } catch (e) {}
+      }
+
+      return key ? jar[key] : jar
+    }
+
+    return Object.create(
+      {
+        set: set,
+        get: get,
+        remove: function (key, attributes) {
+          set(
+            key,
+            '',
+            assign({}, attributes, {
+              expires: -1
+            })
+          );
+        },
+        withAttributes: function (attributes) {
+          return init(this.converter, assign({}, this.attributes, attributes))
+        },
+        withConverter: function (converter) {
+          return init(assign({}, this.converter, converter), this.attributes)
+        }
+      },
+      {
+        attributes: { value: Object.freeze(defaultAttributes) },
+        converter: { value: Object.freeze(converter) }
+      }
+    )
+  }
+
+  var api = init(defaultConverter, { path: '/' });
+  /* eslint-enable no-var */
+
+  return api;
+
+})));
+
+
+
 let main = document.querySelector("main");
 
-const api = new Api("    ");
+const api = new Api("alexander-biryukov-42");
 
 const updCards = function (data) {
   main.innerHTML = "";
@@ -129,48 +279,14 @@ closePopupForm1.addEventListener("click", () => {
   popupForm1.parentElement.classList.remove("active");
 });
 
-
-// 
-// 
-// let addBtn1 = document.querySelector("#btn-enter");
-// let popupForm1 = document.querySelector("#form-login");
-// let closePopupForm1 = popupForm.querySelector(".btn-close_login");
-
-// addBtn1.addEventListener("click", function (evt) {
-// 	const elPopup = formLogin.closest(".popup-wrapper")
-
-//   if (!popupForm.classList.contains("active")) {
-//     popupForm.classList.add("active");
-//     popupForm.parentElement.classList.add("active");
-//   }
-// });
-// closePopupForm1.addEventListener("click", () => {
-//   popupForm1.classList.remove("active");
-//   popupForm1.parentElement.classList.remove("active");
-// });
+foo = 
+Cookies.set('foo', 'bar')
+Cookies.set('foo2', 'bar2')
+Cookies.set('foo3', 'bar3')
+const cookieValue = Cookies.get('foo')
+const cookieValue2 = Cookies.get('foo2')
+const cookieValue3 = Cookies.get('foo3')
+console.log(cookieValue, cookieValue2, cookieValue3)
 
 
-// // /** Открываем форму входа */
-
-// // const btnEnter = document.querySelector('#btn-enter');
-// // const formLogin = document.querySelector('#form-login');
-// // const closeFormLogin = document.querySelector('.btn-close_login')
-
-// // btnEnter.addEventListener('click', function (evt) {
-// // 	// ищем ближайшего предка, подходящего под указанный CSS-селектор
-// // 	// и сохраняем его в переменную.
-// // 	const elPopup = formLogin.closest(".popup-wrapper")
-
-// // 	if (!elPopup.classList.contains("popup_active")) {
-// // 		elPopup.classList.add("popup_active")
-// // 	}
-// // })
-
-// // /** закрываем форму входа */
-// // closeFormLogin.addEventListener('click', function () {
-// // 	const elPopup = formLogin.closest(".popup-form")
-// // 	if (elPopup.classList.contains('popup_active')) {
-// // 		elPopup.classList.remove("popup_active")
-// // 	}
-// // })
 
